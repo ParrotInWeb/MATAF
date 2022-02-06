c89ed7fb0cbb869c80fe977364487f6da8aa32d8
Tomasz Kozik <biuro@funqa.pl>
2022-02-06 06:37:06 +0100

    Set basic cucumber configuration

---
+++ src/test/java/cucumberRunner/Runner.java
@@ -0,0 +1,12 @@
+package cucumberRunner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/",
+        glue = {"stepDefinitions"},
+        plugin = {"pretty"}
+)
+public class Runner extends AbstractTestNGCucumberTests {
+}
