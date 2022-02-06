93f47347e6274412a855b20d8fe2448b1e89d584
Tomasz Kozik <biuro@funqa.pl>
2022-02-06 09:57:32 +0100

    Added AppProperties.java and moved app.properties from test to main

---
+++ src/main/java/utils/AppProperties.java
@@ -0,0 +1,41 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+
+public class AppProperties {
+    private static Properties properties = null;
+
+    public static String getProperty(String key) {
+        return getProperties().getProperty(key);
+    }
+
+    private static Properties getProperties() {
+        if (properties == null) {
+            properties = new Properties();
+            loadProperties(getThePropertiesUrl());
+        }
+        return properties;
+    }
+
+    private static URL getThePropertiesUrl() {
+        String propertiesFileName = "properties/app.properties";
+        URL propertiesUrl = AppProperties.class.getClassLoader().getResource(propertiesFileName);
+        if (propertiesUrl == null) {
+            throw new NullPointerException("AppProperties: File '" + propertiesFileName + "' doesn't exist");
+        }
+        return propertiesUrl;
+    }
+
+    private static void loadProperties(URL preferencesURL) {
+        try {
+            InputStream inputStream = new FileInputStream(preferencesURL.getPath());
+            properties.load(inputStream);
+        } catch (final IOException e) {
+            System.out.println("AppProperties: Exception while with reading application properties file: " + e);
+        }
+    }
+}
