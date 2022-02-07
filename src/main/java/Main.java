768300c273adad88facd1955bca30aed491b77bb
Tomasz Kozik <biuro@funqa.pl>
2022-02-07 07:52:58 +0100

    Added log4j dependencies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
 import helpers.AppiumServer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final AppiumServer appiumServer = new AppiumServer();
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        appiumServer.startAppiumServer();
-        appiumServer.stopAppiumServer();
+        logger.error("Some error");
     }
 }
