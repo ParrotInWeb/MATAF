973cf121f7ef7176ee3ff8dcddf816556c5cfb79
Tomasz Kozik <biuro@funqa.pl>
2022-02-10 06:16:50 +0100

    Added AdbCmd utils

---
+++ src/main/java/examples/AdbCmdExample.java
@@ -0,0 +1,10 @@
+package examples;
+
+import utils.AdbCmd;
+
+public class AdbCmdExample {
+
+    public static void main(String[] args) {
+        AdbCmd.connect();
+    }
+}
---
+++ src/main/java/utils/AdbCmd.java
@@ -0,0 +1,44 @@
+package utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+import static utils.CmdExecutor.execCmd;
+import static utils.CmdExecutor.getResult;
+
+public class AdbCmd {
+
+    private static final String appiumDevice = AppProperties.getProperty("appium.device");
+    private static final Logger logger = LogManager.getLogger(AdbCmd.class);
+
+    public static void connect() {
+        String log = "Wait until device " + appiumDevice + " is connected";
+        logger.info(log);
+        await(log)
+                .atMost(60, TimeUnit.SECONDS)
+                .pollInterval(5, TimeUnit.SECONDS)
+                .until(() -> {
+                    String deviceList = connectToDevice();
+                    if (deviceList.contains(appiumDevice + "offline")) {
+                        logger.error("Device " + appiumDevice + " is offline. Try to reconnect");
+                        disconnect();
+                    }
+                    return deviceList.contains(appiumDevice + "device");
+                });
+    }
+
+    public static void disconnect() {
+        execCmd("adb disconnect");
+    }
+
+    public static String connectToDevice() {
+        if (appiumDevice.contains(".")) {
+            execCmd("adb connect " + appiumDevice);
+        }
+        return getResult(execCmd("adb devices"))
+                .replaceAll("[ \t\n]", "");
+    }
+}
