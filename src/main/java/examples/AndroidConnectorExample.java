27e6ff5d17d60244fc99371ea84c1b9ae23f6acb
Tomasz Kozik <biuro@funqa.pl>
2022-02-12 06:17:34 +0100

    Static AppiumServer

--- src/main/java/examples/AndroidConnectorExample.java
+++ src/main/java/examples/AndroidConnectorExample.java
@@ -1,6 +1,6 @@
 package examples;
 
-import helpers.AppiumServer;
+import utils.AppiumServer;
 import io.appium.java_client.android.AndroidDriver;
 import utils.AdbCmd;
 import utils.Android.AndroidDriverConnector;
@@ -9,14 +9,13 @@ public class AndroidConnectorExample {
 
     public static void main(String[] args) {
         // If you already use Appium Server via Appium desktop, you do not need the internal Appium Server.
-        AppiumServer appiumServer = new AppiumServer();
-        appiumServer.startAppiumServer();
+        AppiumServer.startAppiumServer();
         // If you are sure you are connected to the device, you don't need ADB utils.
         AdbCmd.connect();
         AndroidDriverConnector.connect();
         AndroidDriver androidDriver = AndroidDriverConnector.driver;
         AndroidDriverConnector.disconnect();
         AdbCmd.disconnect();
-        appiumServer.stopAppiumServer();
+        AppiumServer.stopAppiumServer();
     }
 }
